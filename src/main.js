992577ce5596416e9fe306155284559a440bd46a
Angelica Garcia <anyegar36@gmail.com>
2022-10-12 10:47:18 -0500

    Avance HTML, CSS Header y Menu Lateral

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const $ = (selector) => document.querySelector(selector)
+$('.btn-filtros').addEventListener('click', () => {
+  $('.menu-lateral').classList.toggle('menu-lateral-activo')
+})
+
 console.log(example, data);
