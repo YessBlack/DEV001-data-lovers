28808fb153bb2dcf5efb0cdbf34d57d50fe945a0
Angelica Garcia <anyegar36@gmail.com>
2022-10-13 15:42:40 -0500

    Diseño adaptable

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
+import { tarjetas } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(data)
+ //Colocar las imagenes de los personajes en HTML
+ const cards= document.querySelector(".cards");
+
+ //Retorna el resultado de la fc de tarjetas, para realizarla
+ data.results.forEach(personaje => {
+   cards.insertAdjacentHTML("beforeend",tarjetas(personaje))
+ })
 
 
