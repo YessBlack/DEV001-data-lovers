39d07bb303487c06001e5adc0e4ed316b93cc1be
Angelica Garcia <anyegar36@gmail.com>
2022-10-13 16:00:44 -0500

    Template card y diseño responsivo

--- src/data.js
+++ src/data.js
@@ -1,17 +1,3 @@
-export  const tarjetas = (personaje) =>{
-  const tarjeta=` <article class="card">
-  <header class="header-card">
-    <img src="${personaje.image}">
-  </header>
-  <div class="card-body spacing">
-   <p>${personaje.species}</p>
-    <p>${personaje.name}</p>
-    <p>${personaje.status}</p>
-  </div>
-</article>`
- return(tarjeta);
-}
-
 export const anotherExample = () => {
   return 'OMG';
 };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
-import { tarjetas } from './data.js';
+import { tarjetas } from './template/Card.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
- //Colocar las imagenes de los personajes en HTML
- const cards= document.querySelector(".cards");
+//Funcion para selectores
+const $ = (selector) => document.querySelector(selector)
+
+//Template
+
+//Retorna el resultado de la fc de tarjetas, para realizarla
 
  //Retorna el resultado de la fc de tarjetas, para realizarla
  data.results.forEach(personaje => {
-   cards.insertAdjacentHTML("beforeend",tarjetas(personaje))
+   $('.cards').insertAdjacentHTML("beforeend",tarjetas(personaje))
  })
 
 
