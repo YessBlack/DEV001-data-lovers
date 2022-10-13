2ccc12c5a0908123eb8afee29cfdd70d90168e4b
Angelica Garcia <anyegar36@gmail.com>
2022-10-12 21:08:52 -0500

    Listar personajes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
+import { card }from './componentes/Tarjeta.js';
 
 const $ = (selector) => document.querySelector(selector)
 $('.btn-filtros').addEventListener('click', () => {
   $('.menu-lateral').classList.toggle('menu-lateral-activo')
 })
 
-console.log(example, data);
+
+
+data.results.forEach(personaje => {
+  $('.cards').insertAdjacentHTML('beforeend',card(personaje))
+})
+
+
