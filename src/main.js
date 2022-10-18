5c1eb3b3352affbcd62105a0159f219a476d8eac
Natioterom <natioterom@gmail.com>
2022-10-18 15:50:33 -0300

    avance test y template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { getData, getDeads, getAlive, getFemale, getMale } from './data.js';
-
+import {tarjetas} from './template/cards.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
-//Colocar las imagenes de los personajes en HTML
+//Variables.
 const cards = document.querySelector(".cards");
 const btnTodos = document.getElementById("todos");
 const btnMale = document.getElementById("hombre");
@@ -11,42 +11,13 @@ const btnAlive = document.getElementById("vivos");
 const btnDead = document.getElementById("muertos");
 const busqueda = document.querySelector(".busqueda")
 
-//Retorna el resultado de la fc de tarjetas, para realizarla
+//Retorna el resultado de la fx de tarjetas, para realizarla
 getData(data).forEach(personaje => {
-  const tarjetas = (personaje) => {
-    const tarjeta =
-      ` <article class="card">
-    <header class="header-card">
-      <img src="${personaje.image}">
-    </header>
-    <div class="card-body spacing">
-     <p>${personaje.species}</p>
-      <p>${personaje.name}</p>
-      <p>${personaje.status}</p>
-    </div>
-  </article>`
-    return (tarjeta);
-  }
   cards.insertAdjacentHTML("beforeend", tarjetas(personaje))
 })
 //Creo un evento para volver a ver las tarjetas de todos los personajes
-btnTodos.addEventListener("click", (e) => {
-  e.preventDefault();
-  const tarjetas = (personaje) => {
-    const tarjeta =
-      ` <article class="card">
-    <header class="header-card">
-      <img src="${personaje.image}">
-    </header>
-    <div class="card-body spacing">
-     <p>${personaje.species}</p>
-      <p>${personaje.name}</p>
-      <p>${personaje.status}</p>
-    </div>
-  </article>`
-    return (tarjeta);
-  }
-  cards.innerHTML = "";
+btnTodos.addEventListener("click", () => {
+   cards.innerHTML = "";
   //Coloco los personajes en las tarjetas
   const resulTodos = getData(data);
   resulTodos.forEach(personaje => {
@@ -54,135 +25,58 @@ btnTodos.addEventListener("click", (e) => {
   })
 })
 // Creo un evento que al realizar click se muestres los personajes filtrados por gender"Male"
-btnMale.addEventListener("click", (e) => {
-  e.preventDefault();
-  const tarjetasMale = (personajeMale) => {
-    const tarjetaMale =
-      ` <article class="card">
-    <header class="header-card">
-      <img src="${personajeMale.image}">
-    </header>
-    <div class="card-body spacing">
-     <p>${personajeMale.species}</p>
-      <p>${personajeMale.name}</p>
-      <p>${personajeMale.status}</p>
-    </div>
-  </article>`
-    return (tarjetaMale);
-  }
-  cards.innerHTML = "";
+btnMale.addEventListener("click", () => {
+   cards.innerHTML = "";
   //Coloco los personajes en las tarjetas
   const resultMale = getMale(data);
   resultMale.forEach(personajeMale => {
-    cards.insertAdjacentHTML("beforeend", tarjetasMale(personajeMale));
+    cards.insertAdjacentHTML("beforeend", tarjetas(personajeMale));
   })
 })
 // Creo un evento que al realizar click se muestres los personajes filtrados por gender "Female"
-btnFemale.addEventListener("click", (e) => {
-  e.preventDefault();
-  const tarjetasFemale = (personajeFemale) => {
-    const tarjetaFemale =
-      ` <article class="card">
-    <header class="header-card">
-      <img src="${personajeFemale.image}">
-    </header>
-    <div class="card-body spacing">
-     <p>${personajeFemale.species}</p>
-      <p>${personajeFemale.name}</p>
-      <p>${personajeFemale.status}</p>
-    </div>
-  </article>`
-    return (tarjetaFemale);
-  }
+btnFemale.addEventListener("click", () => {
   cards.innerHTML = "";
   //Coloco los personajes en las tarjetas
   const resultFemale = getFemale(data);
   resultFemale.forEach(personajeFemale => {
-    cards.insertAdjacentHTML("beforeend", tarjetasFemale(personajeFemale));
+    cards.insertAdjacentHTML("beforeend", tarjetas(personajeFemale));
   })
 })
-
-
-// Creo un evento que al realizar click se muestres los personajes filtrados por status "Alive"
-btnAlive.addEventListener("click", (e) => {
-  e.preventDefault();
-  const tarjetasAlive = (personajeAlive) => {
-    const tarjetaAlive =
-      ` <article class="card">
-    <header class="header-card">
-      <img src="${personajeAlive.image}">
-    </header>
-    <div class="card-body spacing">
-     <p>${personajeAlive.species}</p>
-      <p>${personajeAlive.name}</p>
-      <p>${personajeAlive.status}</p>
-    </div>
-  </article>`
-    return (tarjetaAlive);
-  }
+// Creo un evento que al realizar click se muestres los personajes filtrados por gender "Alive"
+btnAlive.addEventListener("click", () => {
   cards.innerHTML = "";
   //Coloco los personajes en las tarjetas
   const resultAlive = getAlive(data);
   resultAlive.forEach(personajeAlive => {
-    cards.insertAdjacentHTML("beforeend", tarjetasAlive(personajeAlive));
+    cards.insertAdjacentHTML("beforeend", tarjetas(personajeAlive));
   })
 })
 
-
 //Creo un evento al realizar click en el enlace y filtro los personajes por status "Dead" 
-btnDead.addEventListener("click", (e) => {
-  e.preventDefault();
-  //Funcion para filtras los personajes por status "Dead"
-  const tarjetasDead = (personajeDead) => {
-    const tarjetaDead =
-      ` <article class="card">
-  <header class="header-card">
-    <img src="${personajeDead.image}">
-  </header>
-  <div class="card-body spacing">
-   <p>${personajeDead.species}</p>
-    <p>${personajeDead.name}</p>
-    <p>${personajeDead.status}</p>
-  </div>
-</article>`
-    return (tarjetaDead);
-  }
-  cards.innerHTML = "";
+btnDead.addEventListener("click", () => {
+ cards.innerHTML = "";
   //Coloco los personajes en las tarjetas
   const resultDead = getDeads(data);
   resultDead.forEach(personajeDead => {
-    cards.insertAdjacentHTML("beforeend", tarjetasDead(personajeDead));
+    cards.insertAdjacentHTML("beforeend", tarjetas(personajeDead));
   })
 })
 
 //Busqueda de personjaes le agrego evento al input con keyup
-busqueda.addEventListener("keyup", (e) => {
-  e.preventDefault();
+busqueda.addEventListener("keyup", () => {
   //busqueda de personaje con indexOf
   const busqueda = document.querySelector(".busqueda")
   let textoBusqueda = busqueda.value.toLowerCase();
   cards.innerHTML = "";
-      for (let nombre of getData(data)) {
-      let nombrePersonaje = nombre.name.toLowerCase();
-      if (nombrePersonaje.indexOf(textoBusqueda) != -1) {
-        console.log(nombrePersonaje);
-const busquedaPersonajes=
-             ` <article class="card">
-          <header class="header-card">
-            <img src="${nombre.image}">
-          </header>
-          <div class="card-body spacing">
-           <p>${nombre.species}</p>
-            <p>${nombre.name}</p>
-            <p>${nombre.status}</p>
-          </div>
-        </article>`
-         
-        
-        //Coloco los personajes en las tarjetas
-        // const resultAlive = getAlive(data);
-        // resultAlive.forEach(personajeAlive => {
-          cards.insertAdjacentHTML("beforeend", busquedaPersonajes);
-        // })
-      }
-      }});
+  for (let nombre of getData(data)) {
+    let nombrePersonaje = nombre.name.toLowerCase();
+    if (nombrePersonaje.indexOf(textoBusqueda) != -1) {
+        cards.insertAdjacentHTML("beforeend", tarjetas(nombre));
+
+    }
+  }
+});
+
+ console.log(getData(data).sort(function(a,b){
+ if(a.name < b.name){
+ return -1}}))
