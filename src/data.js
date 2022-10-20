5e1d005c0fd20581534c8b288cadef94031edc91
Natioterom <natioterom@gmail.com>
2022-10-20 16:23:49 -0300

    avance menu, test y sort

--- src/data.js
+++ src/data.js
@@ -24,3 +24,30 @@ export const getMale=(data)=>{
   const arrayMale= getData(data).filter(e=>e.gender=="Male");
   return arrayMale
 }
+// //Ordenar los personajes de la A-Z sin filtro
+export const ascendente=(data,opcion)=>{
+  if(opcion=="1" && $('#muertos')){
+  let a=getDeads(data).sort(function(a,b){
+    if(a.name < b.name){
+          return -1}
+    })
+    if(opcion=="1" && $('#vivos')){
+      let a=getAlive(data).sort(function(a,b){
+        if(a.name < b.name){
+               return -1}
+        })
+    return a
+}}}
+// Ordenar los personajes de a Z-A sin filtro
+export const descendente=(data,opcion)=>{
+  if(opcion=="2" && $('#muertos')){
+    let a=getDeads(data).sort(function(a,b){
+      if(a.name > b.name){
+        
+      return -1}
+      })
+      if(opcion=="2" && $('#vivos')){
+        let a=getAlive(data).sort(function(a,b){
+          if(a.name < b.name){
+                     return -1}
+          })}}}
