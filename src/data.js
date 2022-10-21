8542d7fa6dfe88207548c4dc15b4f3622cda9d6a
Angelica Garcia <anyegar36@gmail.com>
2022-10-21 09:25:37 -0500

    funcion sort

--- src/data.js
+++ src/data.js
@@ -27,3 +27,14 @@ export const getFemale =(data) => {
   const dataFemale = getData(data).filter( e => e.gender == "Female")
   return dataFemale
 }
+
+//Ordenar los personajes de a Z-A sin filtro
+export const descendente=(data)=>{
+  const b=getData(data).sort(function(a,b){
+    if(a.name>b.name){
+      return -1
+    }
+  }
+  )
+  return b
+}
